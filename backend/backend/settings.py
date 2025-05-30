a9afeba3e41b816bbd119a7085373ff2f6fe7dfd
adriannebulao <amcbulao@gmail.com>
2025-05-30 23:57:38 +0800

    feat(useraccount): implement register endpoint

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -54,8 +54,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
-    'auth',
+    'django.contrib.staticfiles',
     'parking',
     'useraccount',
     'rest_framework',
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from useraccount.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/register/', CreateUserView.as_view(), name='register'),
+    path('token/', TokenObtainPairView.as_view(), name='token'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api-auth', include('rest_framework.urls')),
 ]
