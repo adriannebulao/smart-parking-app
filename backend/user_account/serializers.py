d0fe9c7abbaacd6d9a821017dd400adf5efa553d
adriannebulao <amcbulao@gmail.com>
2025-05-31 12:53:15 +0800

    feat(user_account): add first_name and last_name to serializer

--- backend/user_account/serializers.py
+++ backend/user_account/serializers.py
@@ -4,9 +4,16 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ('id', 'username', 'password', 'first_name', 'last_name')
+        extra_kwargs = {'password': {'write_only': True},
+                        'first_name': {'required': True},
+                        'last_name': {'required': True}}
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+        )
         return user
