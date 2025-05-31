734bfe07b351da1a3cd467e80fd6daad51bcfe7d
adriannebulao <amcbulao@gmail.com>
2025-05-31 13:08:40 +0800

    feat(parking_location): define parking location model

--- backend/parking_location/models.py
+++ backend/parking_location/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ParkingLocation(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    slots = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.slots}) slots"
