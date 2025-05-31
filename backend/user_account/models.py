bf444082950890b3dcedd5da9d1480e086f96db6
adriannebulao <amcbulao@gmail.com>
2025-05-31 13:36:54 +0800

    feat(user_account): add user profile methods

--- backend/user_account/models.py
+++ backend/user_account/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+
+    def update_password(self, new_password):
+        self.user.set_password(new_password)
+        self.user.save()
+
+    def update_username(self, new_username):
+        self.user.username = new_username
+        self.user.save()
+
+    def update_name(self, first_name=None, last_name=None):
+        if first_name:
+            self.user.first_name = first_name
+        if last_name:
+            self.user.last_name = last_name
+        self.user.save()
+
+    def deactivate(self):
+        self.user.is_active = False
+        self.user.save()
