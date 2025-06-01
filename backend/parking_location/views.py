7c3f48014869e3f7858cb95fc7fef8b7187cb550
adriannebulao <amcbulao@gmail.com>
2025-06-01 13:39:13 +0800

    feat(parking_location): add filtering, searching, ordering when listing parking locations

--- backend/parking_location/views.py
+++ backend/parking_location/views.py
@@ -1,5 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import IsAuthenticated
+
 from .models import ParkingLocation
 from .serializers import ParkingLocationSerializer
 from .permissions import IsAdminOrReadOnly
@@ -9,3 +11,12 @@ class ParkingLocationViewSet(viewsets.ModelViewSet):
     queryset = ParkingLocation.objects.all()
     serializer_class = ParkingLocationSerializer
     permission_classes = (IsAuthenticated, IsAdminOrReadOnly)
+
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter)
+
+    filterset_fields = {'slots'}
+
+    search_fields = ['name']
+
+    ordering_fields = ['id', 'name', 'slots', 'created_at', 'updated_at']
+    ordering = ['id']
